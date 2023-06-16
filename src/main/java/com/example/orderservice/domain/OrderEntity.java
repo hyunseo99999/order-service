86b41d511947d3f700630c663a099e2c8a6a950e
hsshin <hyunseo99999@naver.com>
2023-06-17 05:21:29 +0900

    order-service 자바버전 내림

--- src/main/java/com/example/orderservice/domain/OrderEntity.java
+++ src/main/java/com/example/orderservice/domain/OrderEntity.java
@@ -1,9 +1,9 @@
 package com.example.orderservice.domain;
 
-import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.ColumnDefault;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -12,7 +12,8 @@ import java.util.Date;
 @Table(name = "orders")
 public class OrderEntity implements Serializable {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, length = 120, unique = true)
